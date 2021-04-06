ba509913786718cb1d4dfb6e9beead816b4b12dd
Pedro Amaral <amaralph1995@gmail.com>
2021-04-06 17:03:24 -0300

    Cria botão More details no card Pokemon

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './App.css';
 import pokemons from './data';
 import Pokedex from './Pokedex';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Pokedex </h1>
-      <Pokedex pokemons={pokemons} />
-    </div>
+    <BrowserRouter>
+      <Route path="/" render={() => <Pokedex pokemons={pokemons} />} />
+    </BrowserRouter>
   );
 }
 
